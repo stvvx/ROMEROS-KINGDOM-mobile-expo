4eced54d0648c225a66291015ccf66caea94c5b7
Jerome Steven Rosario <jeromesteven.rosario@tup.edu.ph>
2026-03-22 02:40:40 +0800

    fixed ui with top safe-are

--- frontend/app/(admin)/_layout.tsx
+++ frontend/app/(admin)/_layout.tsx
@@ -1,28 +1,26 @@
 import { Stack } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function AdminLayout() {
   return (
-    <Stack>
-      <Stack.Screen 
-        name="dashboard"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen 
-        name="categories" 
-        options={{ headerShown: false }} 
-      />
-      <Stack.Screen 
-        name="products" 
-        options={{ headerShown: false }} 
-      />
-      <Stack.Screen 
-        name="review" 
-        options={{ headerShown: false }} 
-      />
-      <Stack.Screen 
-        name="users" 
-        options={{ headerShown: false }} 
-      />
-    </Stack>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#020B18' }} edges={['top']}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen 
+          name="dashboard"
+        />
+        <Stack.Screen 
+          name="categories" 
+        />
+        <Stack.Screen 
+          name="products" 
+        />
+        <Stack.Screen 
+          name="review" 
+        />
+        <Stack.Screen 
+          name="users" 
+        />
+      </Stack>
+    </SafeAreaView>
   );
 }
